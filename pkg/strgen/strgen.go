cdb5a81c6a542835dad7c1f298599a057ece8e6a
Dmitry Petrov <freeman210993@gmail.com>
2023-07-05 12:55:25 +0300

    Updated package strgen

--- pkg/strgen/strgen.go
+++ pkg/strgen/strgen.go
@@ -6,15 +6,13 @@ import (
 	"time"
 )
 
-func Generate() string {
-	rand.Seed(time.Now().UnixNano())
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
-		"abcdefghijklmnopqrstuvwxyzåäö" +
-		"0123456789")
-	length := 8
+var dictionary = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+
+func Generate(length int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	var b strings.Builder
 	for i := 0; i < length; i++ {
-		b.WriteRune(chars[rand.Intn(len(chars))])
+		b.WriteRune(dictionary[r.Intn(len(dictionary))])
 	}
 	return b.String()
 }
